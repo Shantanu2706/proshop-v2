23c3f05f182f98f91ee52d5bea64f49c6b3d87fc
shantanu <shantanu.pimprikar@bajajfinserv.in>
2023-10-15 23:05:58 +0530

    get product details endpoint challenge

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,26 +1,24 @@
-import React,{useState, useEffect} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState({});
     const { id: productId } = useParams();
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-        fetchProduct();
-
-    },[productId])
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
     return (
         <>
             <Link className='btn btn-light my-3' to="/">Go Back</Link>
+            {isLoading ? (
+            <h2>Loading...</h2>
+            ) : error ? (
+                    <div>{ error?.data?.message || error.error}</div>
+            ) : (
             <Row>
                 <Col md={5}>
                     <Image src={product.image} alt={product.name} fluid />
@@ -65,6 +63,8 @@ const ProductScreen = () => {
                     </Card>
                 </Col>
             </Row>
+            )}
+
         </>
     )
 }
